24c3ffc3c549714cfd109ebdb0bc4b39f57a7f60
HamSeven <97118441+HamSeven@users.noreply.github.com>
2023-06-22 20:52:31 +0800

    Update main.java

--- main.java
+++ main.java
@@ -10,13 +10,13 @@ package personalassignment_d210231c;
  */
 public class main {
  public static void main(String[] args) {
-    Ebook ebook = new Ebook("Your Name", "Makoto Shin Kai", 35.99, 30, ".pdf");
+    Ebook ebook = new Ebook("Rich Dad Poor Dad", "Albert Einstein", 16.99, 100, ".pdf");
     ebook.openBook();
     
     ebook.downloadBook();
     System.out.println("File format is valid:"+ebook.validateFormat());
 
-    PhysicalBook physicalBook = new PhysicalBook("Harry Potter", "J.K Rowling", 65.99, 5, 1000);
+    PhysicalBook physicalBook = new PhysicalBook("Wolf", "AAA Hello", 17.99, 3, 1000);
     System.out.println(physicalBook.calculateShippingCost());
     System.out.println(physicalBook.getWeightInPounds());
 }   
